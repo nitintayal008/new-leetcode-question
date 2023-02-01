5c6b9f02935777ac28d7531d48096037f3b99308
Nitin _Tayal <67607648+nitintayal008@users.noreply.github.com>
2023-02-01 19:16:03 +0530

    Time: 6 ms (50.18%), Space: 9.1 MB (13.39%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        string s = "";
+        if( (str1+str2) != (str2+str1) ) {
+            s = "";
+        }
+        else if(str1 == str2) {
+            s = str1;
+        }
+        else if(str1.size() > str2.size()) {
+            s = gcdOfStrings(str1.substr(str2.size()), str2);
+        }
+        else if(str2.size() > str1.size()) {
+            s = gcdOfStrings(str2.substr(str1.size()), str1);
+        }
+        return s;
+    }
+};
